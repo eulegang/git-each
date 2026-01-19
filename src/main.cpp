f7a22ccbabac1555407971feb9bf8aa7b7393aae
eulegang <eulegang@eulegang.dev>
2026-01-18 21:16:57 -0600

    Adding some output on main

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,17 @@
 int main(int argc, char *argv[]) {
   Cli cli(argc, argv);
 
+  if (cli.help) {
+    std::cout << "git each [-s | --system] [-V | --version] [-d | --directory "
+                 "<dir>] <cmd>"
+              << std::endl;
+    return 0;
+  }
+
+  if (cli.args.empty()) {
+    std::cerr << "no command was given" << std::endl;
+    return 1;
+  }
+
   std::cout << cli << std::endl;
 }
